23f76bd1f502b40241276d6bb99060f40f4e55c7
Hannah MacCallum <hmaccallum@optiseller.com>
2025-10-26 20:53:45 +0000

    Create a sauna entity to define structure for the list of saunas populated for the map

---
+++ UXDesignAssignment/Models/Class.cs
@@ -0,0 +1,19 @@
+﻿namespace UXDesignAssignment.Models
+{
+    public class Sauna
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Website { get; set; }
+        public string Heat { get; set; } = "";
+        public bool HasOpenWater { get; set; }
+        public bool HasPlunge { get; set; }
+        public double Rating { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
--- UXDesignAssignment/obj/Debug/net8.0/UXDesignAssignment.AssemblyInfo.cs
+++ UXDesignAssignment/obj/Debug/net8.0/UXDesignAssignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UXDesignAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38964edde1bb2e311c052ae8965f70bf0570d855")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b6c94760bcc568248bb1a19f96d53926abdff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UXDesignAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UXDesignAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
