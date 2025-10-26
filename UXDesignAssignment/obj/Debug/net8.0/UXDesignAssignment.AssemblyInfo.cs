d9e83f032dac976cde710cf508944bdc237c98fc
Hannah MacCallum <hmaccallum@optiseller.com>
2025-10-26 14:32:58 +0000

    Add sauna history to about page

--- UXDesignAssignment/obj/Debug/net8.0/UXDesignAssignment.AssemblyInfo.cs
+++ UXDesignAssignment/obj/Debug/net8.0/UXDesignAssignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UXDesignAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098ef7a4ba5d574148e938598534cdbcd2902ed4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01fda9f4e168a9dd1ae2c1cff77f62636fc50b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UXDesignAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UXDesignAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
